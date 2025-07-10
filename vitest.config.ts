6a6f7e1931b60685dfe600cc41cfbcdfaf30a4f9
naronchen <naronchen@users.noreply.github.com>
2025-07-10 14:48:29 -0400

    config clean

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,8 @@
 import { defineConfig } from 'vitest/config';
 import dotenv from 'dotenv';
 
-// load your tests/.env before Vitest runs
 dotenv.config({ path: './tests/.env' });
 
 export default defineConfig({
-  // if you want to use envDir for code under test:
   envDir: './tests',
 });
